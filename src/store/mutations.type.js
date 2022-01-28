8a351f76a3659c1fb1a319934f6a7aa483ce14c8
Jop <jophuijbers2001@gmail.com>
2022-01-28 22:44:33 +0100

    Fetch playlists

--- src/store/mutations.type.js
+++ src/store/mutations.type.js
@@ -1,1 +1,2 @@
+
 export const SET_PLAYLISTS = 'setPlaylists'
--- src/store/spotify/actions.js
+++ src/store/spotify/actions.js
@@ -1,3 +1,4 @@
+import ApiService from "../../services/api.service";
 import {
     FETCH_PLAYLISTS
 } from '../actions.type'
@@ -5,10 +6,15 @@ import {
     SET_PLAYLISTS
 } from '../mutations.type'
 
-
 const actions = {
     async [FETCH_PLAYLISTS] (context) {
-        context.commit(SET_PLAYLISTS, ['hoi','hoi2'])
+        ApiService.setHeader()
+        try {
+            const {data} = await ApiService.get('/me/playlists')
+            context.commit(SET_PLAYLISTS, data.items)
+        } catch ({ response }) {
+            console.log(response)
+        }
     },
 }
 
